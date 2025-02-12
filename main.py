ee6903f3f4b75d766f732ad925fd194f26eb4da3
Vibhanshu Singh <135852806+singhvibhanshu@users.noreply.github.com>
2025-02-12 17:49:53 +0530

    updated

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+
+WIDTH, HEIGHT = 700, 800
+
+win = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("MineSweeper")
+
+def main():
+    run = True
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+
+if __name__ == "__main__":
+    main()
